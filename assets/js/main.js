29de5c5805716ff4705f527eea71bcd5f3428346
Lê Khả Đức Anh <ducanhlekha2802@gmail.com>
2023-12-12 10:15:35 +0700

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,6 +80,10 @@ $(function() {
                 break
             
             case 1600:
+                $('#content-preview').animate({
+                    scrollTop: 0
+                }, 'smooth');
+                swiper2.params.slidesPerView = 3
                 handleLayoutInPC()
                 break
             default:
@@ -172,9 +176,92 @@ $(function() {
             fontSize: '10px'
         })
     }
-    
+ 
     const handleLayoutInPC = () => {
-    
+        $('.main-content .content-preview').css({
+            display: '',
+            justifyContent: ''
+        })
+        $('.main-content .content-preview .content-container').css({
+            width: ''
+        })
+        $('.content-preview .content-container .content-header .header-layout').css({
+            height: ''
+        })
+        $('.content-container .content-header .header-layout > div:first-child img').css({
+            height: ''
+        })
+        $('.content-preview .content-container .content-header .header-layout > div:not(.header-e-profile-name) h2').css({
+            fontSize: ''
+        })
+        $('.content-preview .content-container .content-header .header-layout > div:nth-child(2) h2').css({
+            fontSize: ''
+        })
+        $('.content-container .content-slider-image .slider-banner-container .slider-box img').css({
+            height: ''
+        })
+        $('.content-container .content-slider-image').css({
+            height: ''
+        })
+        $('.content-sapo, .content-title').css({
+            padding: ''
+        })
+        $('.content-sapo .sapo-detail .sapo-text, .content-partner-information .partner-introduce .introduce, .content-partner-information .partner-information ul li, .content-description .description, .content-video .description, .content-product-container .product-card .product-description .product-introduce').css({
+            fontSize: '',
+            lineHeight: ''
+        })
+        $('.content-sapo .sapo-detail .sapo-text .sapo-quote').css({
+            fontSize: '',
+        })
+        $('.content-title, .content-box').css({
+            marginTop: '',
+        })
+        $('.content-title .title').css({
+            fontSize: '',
+        })
+        $('.content-partner-information .partner-introduce, .content-partner-information .partner-information, .content-video .description, .content-video .video').css({
+            gridColumn: ''
+        })
+        $('.content-box .about-us').css({
+            fontSize: '',
+            marginBottom: ''
+        })
+        $('.content-product-container .product-card .product-image').css({
+            height: ''
+        })
+        $('.content-product-container .product-card .product-description .product-name').css({
+            fontSize: '',
+        })
+        $('.content-product-layout .content-product-container').css({
+            padding: ''
+        })
+        $('.partner-contact-container').css({
+            margin: ''
+        })
+        $('.partner-contact-container .partner-address-map, .partner-contact-container .partner-form').css({
+            gridColumn: ''
+        })
+        $('.partner-contact-container .partner-address-map').css({
+            height: ''
+        })
+        $('.content-social-medial-container').css({
+            display: '',
+            gap: '',
+            gridTemplateColumns: ''
+        })
+        $('.content-social-medial-container .social-link').css({
+            gridColumn: '',
+            fontSize: '',
+            justifyContent: '',
+            gap: '',
+            marginLeft: ''
+        })
+        $('.content-box .content-social-medial-container .social-link i').css({
+            fontSize: ''
+        })
+        $('.content-box .content-copyright').css({
+            fontSize: ''
+        })
     }
 
 });
